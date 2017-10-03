3ec605b766bf8b5263baa2c820c38817205b9630
Hun Howard Kim <codinghunkim@gmail.com>
2017-10-03 13:03:28 -0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         '2nNh6p6em2ulrlqabYPKxOrtC',
-  consumer_secret:      '9Z8pNb1xP8sNIqpQKnjOCtq4zZCoE3Hv3sKMxuI9i8lwI6giSe',
-  access_token:         '913915383941185536-v6q6dq2r2LGhSZVEBSLgqp04gC5JnS5',
-  access_token_secret:  'TztgEdECUCBot87CRqg869kGk7I1o8kq7xTDvRgX4o2ci'
-}
